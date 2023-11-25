4668250cb8e57c62ed5ab9a4944500b08e05deef
nikhilchauhangithub <thisisnikhilchauhan@gmail.com>
2023-11-25 10:54:38 +0530

    name error

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -1,41 +1,48 @@
-"use client";
+'use client';
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 const testimonials = [
   {
-    name: "Joel",
-    avatar: "J",
-    title: "Software Engineer",
+    name: 'Yogesh',
+    avatar: 'J',
+    title: 'Software Engineer',
     description: "This is the best application I've ever used!",
   },
   {
-    name: "Antonio",
-    avatar: "A",
-    title: "Designer",
-    description: "I use this daily for generating new photos!",
+    name: 'Palak',
+    avatar: 'A',
+    title: ' Designer',
+    description: 'I use this daily for generating new photos!',
   },
   {
-    name: "Mark",
-    avatar: "M",
-    title: "CEO",
-    description: "This app has changed my life, cannot imagine working without it!",
+    name: 'Nikhil Chauhan',
+    avatar: 'M',
+    title: 'CEO',
+    description:
+      'This app has changed my life, cannot imagine working without it!',
   },
   {
-    name: "Mary",
-    avatar: "M",
-    title: "CFO",
-    description: "The best in class, definitely worth the premium subscription!",
+    name: 'Ujjwal',
+    avatar: 'M',
+    title: 'CFO',
+    description:
+      'The best in class, definitely worth the premium subscription!',
   },
 ];
 
 export const LandingContent = () => {
   return (
     <div className="px-10 pb-20">
-      <h2 className="text-center text-4xl text-white font-extrabold mb-10">Testimonials</h2>
+      <h2 className="text-center text-4xl text-white font-extrabold mb-10">
+        Testimonials
+      </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {testimonials.map((item) => (
-          <Card key={item.description} className="bg-[#192339] border-none text-white">
+          <Card
+            key={item.description}
+            className="bg-[#192339] border-none text-white"
+          >
             <CardHeader>
               <CardTitle className="flex items-center gap-x-2">
                 <div>
@@ -51,5 +58,5 @@ export const LandingContent = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
